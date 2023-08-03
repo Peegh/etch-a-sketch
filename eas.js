9da04a177025695af210fb540fd78fdc87259d4f
Peter Tran <79554073+Peegh@users.noreply.github.com>
2023-08-03 20:43:56 +1000

    Create functions to calculate grid dimensions and then generate the grid with those dimensions

--- eas.js
+++ eas.js
@@ -2,13 +2,34 @@ const container = document.querySelector('#container');
 
 const btn = document.querySelector('#btn');
 
-// btn.addEventListener('click', () => {
-//     var innerDiv = document.createElement('div');
-//     innerDiv.classList.add('item');
-//     container.appendChild(innerDiv);
-// });
+var boxHeight;
+var boxWidth;
+var gridAmount;
+
 //Read User Input Number for grid size
 btn.addEventListener('click', () => {
     let gridSize = prompt("Please Enter Grid Size(Max 100):")
+    calculateGrid(gridSize);
+    generateGrid();
 })
+
+//Calculate Size of Grid and Box Dimensions
+function calculateGrid(gridSize) {
+    boxHeight = 900/gridSize;
+    boxWidth = 900/gridSize;
+    gridAmount = gridSize * gridSize;
+}
+
+//Generate Grid using the calculated dimensions
+function generateGrid() {
+    for (i = 0; i < gridAmount; i++) {
+        var innerDiv = document.createElement('div');
+        innerDiv.classList.add('item');
+        innerDiv.setAttribute('style', `height: ${boxHeight}px; width: ${boxWidth}px`);
+        console.log(boxHeight);
+        console.log(boxWidth);
+        container.appendChild(innerDiv);
+    }
+}
+
 
