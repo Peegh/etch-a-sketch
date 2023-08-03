a6c67e3f34dade5f271ee0a7feeae17cd2b6beb6
Peter Tran <79554073+Peegh@users.noreply.github.com>
2023-08-03 20:06:21 +1000

    Create User Prompt to enter grid size

---
+++ eas.js
@@ -0,0 +1,14 @@
+const container = document.querySelector('#container');
+
+const btn = document.querySelector('#btn');
+
+// btn.addEventListener('click', () => {
+//     var innerDiv = document.createElement('div');
+//     innerDiv.classList.add('item');
+//     container.appendChild(innerDiv);
+// });
+//Read User Input Number for grid size
+btn.addEventListener('click', () => {
+    let gridSize = prompt("Please Enter Grid Size(Max 100):")
+})
+
